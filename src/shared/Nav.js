a8085e7c3eb9883a5a50a483b04aed1036693f3e
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-10-26 12:52:46 -0700

    Just a little styling to center things

--- src/shared/Nav.js
+++ src/shared/Nav.js
@@ -2,28 +2,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 // import React
-import React from 'react'
+import React from 'react';
 // import css
-import './Nav.css'
+import './Nav.css';
 
 function Nav() {
-    const handleClick = () => {
-        let nav = document.getElementById('linksList')
-        if (nav.style.display === 'flex') {
-            nav.style.display = 'none'
-        } else {
-            nav.style.display = 'flex'
-        }
-    }
-    return (
-			<header>
+	const handleClick = () => {
+		let nav = document.getElementById('linksList');
+		if (nav.style.display === 'flex') {
+			nav.style.display = 'none';
+		} else {
+			nav.style.display = 'flex';
+		}
+	};
+	return (
+		<>
+			<header className='navBar'>
 				<div className='appTitle'>PALATE</div>
 				<div className='navIcon' onClick={handleClick}>
-					<FontAwesomeIcon icon={faBars} size='3x' />
+					<FontAwesomeIcon icon={faBars} size='2x' />
 				</div>
-				<nav id='linksList'>About</nav>
 			</header>
-		);
+			<nav id='linksList'>About</nav>
+		</>
+	);
 }
 
-export default Nav
+export default Nav;
