4c2940dde46fb81d54834e9c0b6352faa0545f07
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-10-28 21:38:02 -0700

    Updated Rating List

--- src/components/RatingList/RatingList.js
+++ src/components/RatingList/RatingList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from '../Rating/Rating'
 import RatingForm from '../RatingForm/RatingForm';
 import { Route, Switch } from 'react-router-dom';
 // import font awesome
@@ -94,19 +95,8 @@ function RatingList(props) {
 			</>
 		);
 	}
-
-	const ratings = props.searchedItem;
-	let ratingToDisplay = 'Loading...';
-	if (props.searchedItem[0]) {
-		ratingToDisplay = ratings.map((rating) => {
-			<div>
-				<p>Name: {rating.ratings[0].name}</p>
-				<p>Date: {rating.ratings[0].date}</p>
-				<p>Rating: {stars(rating.ratings[0].stars)}</p>
-				<p>Comment: {rating.ratings[0].comment}</p>
-			</div>;
-		});
-	}
+	// will need to check pathing
+	const id = props.match.params.id
 
 	const url = 'https://aa-palate-backend.herokuapp.com/';
 
@@ -119,8 +109,27 @@ function RatingList(props) {
 		comment: '',
 	};
 
+	const ratings = props.searchedItem;
+
+	const [selectedRating, setSelectedRating] = React.useState(emptyItem);
+
 	const [form, setForm] = React.useState(emptyRating);
 
+	const [newRatingState, setNewRatingState] = React.useState([])
+
+	let ratingToDisplay = 'Loading...';
+	if (props.searchedItem[0]) {
+		ratingToDisplay = ratings.map((rating) => {
+			<div>
+				<p>Name: {rating.ratings[0].name}</p>
+				<p>Date: {rating.ratings[0].date}</p>
+				<p>Rating: {stars(rating.ratings[0].stars)}</p>
+				<p>Comment: {rating.ratings[0].comment}</p>
+			</div>;
+		});
+	}
+
+	// Get List of Ratings (I think all)
 	const getRatings = () => {
 		fetch(url + 'ratings/')
 			.then((response) => response.json())
@@ -129,6 +138,22 @@ function RatingList(props) {
 			});
 	};
 
+	// Get function to render
+	const getItemRating = () => {
+		setNewRatingState(props.searchedItem)
+	}
+
+	// Renders first time on page
+	React.useEffect(() => getItemRating(), [])
+
+	// updates List after change
+	const updateRatingList = () => {
+		fetch(url + 'ratings/' + props.match.params.id)
+		.then((response) => response.json())
+		.then((data) => {setNewRatingState([data.items])})
+	}
+
+	// Creates a rating
 	const handleCreate = (newRating) => {
 		let payload = { newRating, restId: props.match.params._id };
 
@@ -139,10 +164,11 @@ function RatingList(props) {
 			},
 			body: JSON.stringify(payload),
 		}).then(() => {
-			getRatings();
+			updateRatingList();
 		});
 	};
 
+	// Handles edit
 	const handleUpdate = (rating) => {
 		fetch(url + 'ratings/' + rating._id, {
 			method: 'put',
@@ -158,14 +184,28 @@ function RatingList(props) {
 	const deleteRating = (rating) => {
 		fetch(url + ':id/' + rating._id, {
 			method: 'delete',
-		}).then((response) => getRatings());
+		})
+		return updateRatingList()
 	};
+
+	// adds Item name to top of page
+	let iName = props.searchedItem
+	let itemName = "loading..."
+	if (props.searchedItem[0]) {
+		itemName = iName.map((item) => {
+			return (
+				<div>
+					<p>{item.name}</p>
+					<hr />
+				</div>
+			)
+		})
+	}
 
 	return (
 		<>
 			<h2>This is the RatingList Component</h2>
 			{ratingToDisplay}
-			<Switch>
 				<Route
 					path='/rating/:id'
 					render={(routerprops) => (
@@ -176,7 +216,12 @@ function RatingList(props) {
 						/>
 					)}
 				/>
-				<Route
+				<Rating 
+					newRatingState={newRatingState}
+					selectRating={selectedRating}
+					deleteRating={deleteRating}
+				/>
+				{/* <Route
 					path='/rating/:id'
 					render={(routerprops) => (
 						<RatingForm
@@ -185,8 +230,7 @@ function RatingList(props) {
 							rating={form}
 						/>
 					)}
-				/>
-			</Switch>
+				/> */}
 		</>
 	);
 }
