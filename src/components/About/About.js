282d418851b9abf6e94877ba6256973c7b769864
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-12-11 19:19:02 -0500

    Updates to About Component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -25,25 +25,26 @@ function About() {
 			<div className="bio-container">
 				<h1>Team</h1>
 				<img src={adeolaPhoto} alt="Adeola bio photo" className="team"/>
-					<p className="name">Adeola</p>
+					<p className="name"><a href="https://www.linkedin.com/in/adeola-a-b0b6b270/" target="_blank">Adeola</a></p>
+				
 					<p className="bio-text">Hi! I really enjoyed seeing all the core fundamentals
                         learned throughout this course so far come together in
                         this full stack build. I love diagnosing real life
                         problems and thinking through ways to solve them through
                         code and applications. For this build, I primarily
                         worked on the backend ensuring our data was accessible on
-                        the front-end.</p>
+                        the front-end.</p> 
 				</div>
 
 				<div className="bio-container">
 				<img src={leahPhoto} alt="Leah bio photo" className="team"/>
-					<p className="name">Leah</p>
+					<p className="name"><a href="https://www.linkedin.com/in/leah-haake/" target="_blank">Leah</a></p>
 					<p className="bio-text">I'm a web developer with a background in public health. I'm excited to be learning full stack web development and to continue to develop my programming skills. I worked on the front-end implementing CRUD functionality for our full stack application.</p>
 				</div>
 
 				<div className="bio-container">
 				<img src={jessePhoto} alt="Jesse bio photo" className="team"/>
-					<p className="name">Jesse</p>
+					<p className="name"><a href="https://www.linkedin.com/in/jesse-rouse-92a7aa1b1/" target="_blank">Jesse</a></p>
 					<p className="bio-text">I’m a software engineer who has used my experience in education and retail to pay attention to detail when troubleshooting and thinking through code. I worked primarily on the frontend setting up foundations to be fined tuned later. I worked on the non crud functionality behind the scenes on the site to make the little details work.</p>
 				</div>
 		</main>
